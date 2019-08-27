e8d38848ee4658fc221a6979804e31c350acd561
Yaroslav <yaroslav.chelentano@gmail.com>
2019-08-27 05:42:41 +0300

    Add class Person(task 3)

---
+++ 3Task1_2/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab3task3
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public IEnumerable<string> PhoneNumbers { get; set; }
+    }
+}
